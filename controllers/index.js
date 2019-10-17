7c76ab123b457e3eeadb18c86617f31ab7141975
KURTALIQI Ajtene <akurtaliqi@myges.fr>
2019-10-17 09:43:12 +0200

    [PRODUCT] implement CRUD

--- controllers/index.js
+++ controllers/index.js
@@ -12,4 +12,5 @@ module.exports = {
     GameLevelController: require('./gameLevel.controller'),
     ScoreController: require('./score.controller'),
     MediaController: require('./media.controller'),
+    ProductController:require('./product.controller'),
 };
